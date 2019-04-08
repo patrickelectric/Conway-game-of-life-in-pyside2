5c887f620cc7dda7e09533e63d3862d79e36d981
Patrick José Pereira <patrickelectric@gmail.com>
2019-04-08 17:12:43 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ magic.py
@@ -0,0 +1,29 @@
+from PySide2.QtCore import QObject, Qt, Slot, Property, Signal, QTimer
+from PySide2.QtQml import qmlRegisterType
+
+import copy
+import pickle
+
+class Magic(QObject):
+    _signals = {}
+    _attributes = {}
+
+    def __init__(self, parent=None):
+        super(Magic, self).__init__(parent)
+        self._foo = False
+
+    def foo(self):
+        return self._foo
+
+    def setFoo(self, value):
+        self._foo = value
+        self.fooChanged.emit()
+
+    fooChanged = Signal()
+    foo = Property(bool, foo, setFoo, notify=fooChanged)
+
+    @Slot()
+    def print_it(self):
+        print('>', self._foo)
+
+qmlRegisterType(Magic, 'Magic', 1, 0, 'Magic')
